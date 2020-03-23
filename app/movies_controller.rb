de28451f6253d6980714716cb1e79b959f060c72
arnie b <arnold.bwaila@gmail.com>
2020-03-23 17:15:24 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -47,7 +47,8 @@ def can_get_size_of_the_database
   Movie.all.size
 end
 
-def can_find_the_first_item_from_the_database_using_id(id)
+def can_find_the_first_item_from_the_database_using_id
+  id = Movie.minimum(id:)
   Movie.find_by(id: id)
 end
 
